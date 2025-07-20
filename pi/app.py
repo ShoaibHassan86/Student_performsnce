27ef03571c608e4cd359a69a0eaecde246ae6f8d
Shoaib Hassan <shoaib.it86@gmail.com>
2025-07-20 20:14:38 +0500

    Update app.py

--- pi/app.py
+++ pi/app.py
@@ -2,13 +2,19 @@ import streamlit as st
 import pandas as pd
 import pickle# Load model
 import os
+import os
+import streamlit as st
+
 def load_local_css(file_name):
     try:
         with open(file_name, "r") as f:
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     except FileNotFoundError:
         st.warning("⚠️ style.css not found. Default styling applied.")
-        
+
+css_path = os.path.join(os.path.dirname(__file__), 'style.css')
+load_local_css(css_path)
+
 model_path = os.path.join(os.path.dirname(__file__), 'performance_index.sav')
 model = pickle.load(open(model_path, 'rb'))
 
